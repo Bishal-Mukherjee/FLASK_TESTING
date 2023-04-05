7ca4977f76a9a9c8ceebd3b3eabd19acfe3c87f0
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2023-04-05 12:47:17 +0530

    index.py modified for ml

--- api/index.py
+++ api/index.py
@@ -1,16 +1,31 @@
-from flask import Flask
-
+from flask import Flask, app, request, json
+from ml.model import *
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/', methods=['GET'])
+def index():
+    return {'message': 'server working'}
 
 
-@app.route('/about')
-def about():
-    return 'About'
+# @app.route('/', methods=['POST'])
+# def predict():
+#     reqbody = json.loads(request.data)
+#     symptoms = []
+
+#     if len(reqbody['symptoms']) == 0:
+#         return {'message': 'no symptoms provided'}
+
+#     for symptom in reqbody['symptoms']:
+#         try:
+#             if(symptom.index(" ")):
+#                 symptoms.append(symptom.replace(" ", '_'))
+#         except:
+#             symptoms.append(symptom)
+
+#     predictedDiease = NaiveBayes(symptoms)
+
+#     return {'predictedDiease': predictedDiease}
 
 
 if __name__ == "__main__":
